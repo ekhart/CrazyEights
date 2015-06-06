ae7732b11b20341716e89a78f5ed92c2a3766ad4
Artur Dębkowski <arturdebkowski@gmail.com>
2015-06-06 11:09:01 +0200

    drawing in a view

--- app/src/main/java/pl/ekhart/crazyeights/CrazyEightsActivity.java
+++ app/src/main/java/pl/ekhart/crazyeights/CrazyEightsActivity.java
@@ -1,38 +1,16 @@
 package pl.ekhart.crazyeights;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
-public class CrazyEightsActivity extends ActionBarActivity {
+public class CrazyEightsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crazy_eights);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_crazy_eights, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        CrazyEightsView view = new CrazyEightsView(this);
+        setContentView(view);
     }
 }
--- app/src/main/java/pl/ekhart/crazyeights/CrazyEightsView.java
+++ app/src/main/java/pl/ekhart/crazyeights/CrazyEightsView.java
@@ -1,13 +1,35 @@
 package pl.ekhart.crazyeights;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.Pair;
 import android.view.View;
 
 /**
  * Created by Ekh on 2015-06-06.
  */
 public class CrazyEightsView extends View {
+
+    private Paint redPaint;
+    private Pair<Integer, Integer> circlePlace;
+    private float radius;
+
     public CrazyEightsView(Context context) {
         super(context);
+
+        redPaint = new Paint();
+        redPaint.setAntiAlias(true);
+        redPaint.setColor(Color.RED);
+
+        circlePlace = new Pair<>(100, 100);
+        radius = 30;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+//        super.onDraw(canvas);
+        canvas.drawCircle(circlePlace.first, circlePlace.second, radius, redPaint);
     }
 }
