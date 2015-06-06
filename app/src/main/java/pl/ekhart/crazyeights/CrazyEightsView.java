002ee45974ca2991386133f9a34b6de7b369cf7f
Artur Dębkowski <arturdebkowski@gmail.com>
2015-06-06 10:56:20 +0200

    first commit

---
+++ app/src/main/java/pl/ekhart/crazyeights/CrazyEightsView.java
@@ -0,0 +1,13 @@
+package pl.ekhart.crazyeights;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by Ekh on 2015-06-06.
+ */
+public class CrazyEightsView extends View {
+    public CrazyEightsView(Context context) {
+        super(context);
+    }
+}
